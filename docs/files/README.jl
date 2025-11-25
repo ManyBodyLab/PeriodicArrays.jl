dba6b232e46918ebd86289f01422e595c40ee33e
AFeuerpfeil <andreas.feuerpfeil@gmail.com>
2025-11-25 17:15:19 -0500

    sync

--- docs/files/README.jl
+++ docs/files/README.jl
@@ -1,38 +1,5 @@
 # # PeriodicArrays.jl
 
-# | **Documentation** | **Downloads** |
-# |:-----------------:|:-------------:|
-# | [![][docs-stable-img]][docs-stable-url] [![][docs-dev-img]][docs-dev-url] | [![Downloads][downloads-img]][downloads-url]
-
-# | **Build Status** | **PkgEval** | **Coverage** | **Style Guide** | **Quality assurance** |
-# |:----------------:|:-----------:|:------------:|:---------------:|:---------------------:|
-# | [![CI][ci-img]][ci-url] | [![PkgEval][pkgeval-img]][pkgeval-url] | [![Codecov][codecov-img]][codecov-url] | [![code style: runic][codestyle-img]][codestyle-url] | [![Aqua QA][aqua-img]][aqua-url] |
-
-# [docs-stable-img]: https://img.shields.io/badge/docs-stable-blue.svg
-# [docs-stable-url]: https://manybodylab.github.io/PeriodicArrays.jl/stable
-
-# [docs-dev-img]: https://img.shields.io/badge/docs-dev-blue.svg
-# [docs-dev-url]: https://manybodylab.github.io/PeriodicArrays.jl/dev
-
-# [downloads-img]: https://img.shields.io/badge/dynamic/json?url=http%3A%2F%2Fjuliapkgstats.com%2Fapi%2Fv1%2Ftotal_downloads%2FPeriodicArrays&query=total_requests&label=Downloads
-# [downloads-url]: http://juliapkgstats.com/pkg/PeriodicArrays
-
-# [ci-img]: https://github.com/ManyBodyLab/PeriodicArrays.jl/actions/workflows/Tests.yml/badge.svg
-# [ci-url]: https://github.com/ManyBodyLab/PeriodicArrays.jl/actions/workflows/Tests.yml
-
-# [pkgeval-img]: https://JuliaCI.github.io/NanosoldierReports/pkgeval_badges/M/PeriodicArrays.svg
-# [pkgeval-url]: https://JuliaCI.github.io/NanosoldierReports/pkgeval_badges/M/PeriodicArrays.html
-
-# [codecov-img]: https://codecov.io/gh/ManyBodyLab/PeriodicArrays.jl/branch/main/graph/badge.svg
-# [codecov-url]: https://codecov.io/gh/ManyBodyLab/PeriodicArrays.jl
-
-# [aqua-img]: https://raw.githubusercontent.com/JuliaTesting/Aqua.jl/master/badge.svg
-# [aqua-url]: https://github.com/JuliaTesting/Aqua.jl
-
-# [codestyle-img]: https://img.shields.io/badge/code_style-%E1%9A%B1%E1%9A%A2%E1%9A%BE%E1%9B%81%E1%9A%B2-black
-# [codestyle-url]: https://github.com/fredrikekre/Runic.jl
-
-
 # ## Installation
 
 # The package is not yet registered in the Julia general registry. It can be installed trough the package manager with the following command:
