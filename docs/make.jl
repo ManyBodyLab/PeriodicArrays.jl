b8d97f51560343aa16ee5507f3a32c384270e3ad
AFeuerpfeil <andreas.feuerpfeil@gmail.com>
2025-11-25 14:07:26 -0500

    sync

--- docs/make.jl
+++ docs/make.jl
@@ -14,7 +14,7 @@ makedocs(;
     format = Documenter.HTML(;
         canonical = "https://manybodylab.github.io/PeriodicArrays.jl",
         edit_link = "main",
-        assets = ["assets/logo.svg", "assets/extras.css"],
+        assets = ["assets/logo.png", "assets/extras.css"],
     ),
     pages = ["Home" => "index.md", "Reference" => "reference.md"],
 )
